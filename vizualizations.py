d6d6e6a6fd0b1703f2d1a31aff2fbf89e39d36ad
Adi <itida2007@gmail.com>
2020-03-27 19:07:58 -0400

    Added more viz for US and Italy.

--- vizualizations.py
+++ vizualizations.py
@@ -45,7 +45,7 @@ f1.add_subplot(121)
 plt.barh(latest_data1.sort_values('Confirmed')["Confirmed"].index[-8:],latest_data1.sort_values('Confirmed')["Confirmed"].values[-8:],color='Pink')
 plt.tick_params(size=6,labelsize = 13);
 plt.xlabel("Confirmed",fontsize=18);
-plt.title("Top 10 Countries (Confirmed Cases)",fontsize=16);
+plt.title("Top 8 Countries (Confirmed Cases)",fontsize=16);
 #plt.savefig('Top 10 Countries (Confirmed Cases).png')
 plt.grid(alpha=0.23)
 
@@ -54,8 +54,8 @@ f1.add_subplot(122)
 plt.barh(latest_data1.sort_values('Deaths')["Deaths"].index[-8:],latest_data1.sort_values('Deaths')["Deaths"].values[-8:],color='Pink')
 plt.tick_params(size=6,labelsize = 13);
 plt.xlabel("Deaths",fontsize=18);
-plt.title("Top 10 Countries (Deaths Cases)",fontsize=16);
-plt.savefig('Top 10 Countries (Confirmed and Death Cases).png')
+plt.title("Top 8 Countries (Deaths Cases)",fontsize=16);
+plt.savefig('Top 8 Countries (Confirmed and Death Cases).png')
 plt.grid(alpha=0.23)
 
 f2 = plt.figure(figsize=(8.5, 5));
@@ -64,8 +64,8 @@ f2.add_subplot(111)
 plt.barh(latest_data1.sort_values('Recovered')["Recovered"].index[-8:],latest_data1.sort_values('Recovered')["Recovered"].values[-8:],color='Pink')
 plt.tick_params(size=6,labelsize = 13);
 plt.xlabel("Recovered",fontsize=18);
-plt.title("Top 10 Countries (Recovered Cases)",fontsize=16);
-plt.savefig('Top 10 Countries (Recovered Cases).png')
+plt.title("Top 8 Countries (Recovered Cases)",fontsize=16);
+plt.savefig('Top 8 Countries (Recovered Cases).png')
 plt.grid(alpha=0.23)
 
 #################################################################
@@ -93,3 +93,50 @@ chinaD.show()
 chinaR = px.pie(china, values='Recovered', names='State', title = "Statewise Recovered in China")
 chinaR.update_traces(hoverinfo='label+percent+value', textinfo='percent+value', textposition = 'inside')
 chinaR.show()
+
+#################################################################
+####################### TASK 4 ##################################
+#################################################################
+
+# GOAL - To make a grouped bar graph for the statewise counts in US.
+us = latest_data[latest_data.Country == 'US'][['State', 'Confirmed', 'Deaths', 'Recovered']]
+us = us.groupby(['State']).sum().reset_index()
+
+fig1 = go.Figure(data=[
+    go.Bar(name='Confirmed', x=us['State'], y=us['Confirmed']),
+    go.Bar(name='Deaths', x=us['State'], y=us['Deaths']),
+    go.Bar(name='Recovered', x=us['State'], y=us['Recovered'])
+])
+# Change the bar mode - can change to stack in future if necessary.
+fig1.update_layout(barmode='group')
+fig1.show();
+
+
+#################################################################
+####################### TASK 5 ##################################
+#################################################################
+
+# GOAL - To make a time series for the counts for Italy
+italy = covid_19_data[covid_19_data.Country == 'Italy'][['Date', 'Confirmed', 'Deaths', 'Recovered']]
+italy = italy.groupby(['Date']).sum().reset_index()
+
+italyGraph = go.Figure()
+
+italyGraph.add_trace(go.Scatter(x=italy.Date, y=italy['Confirmed'], name="Confirmed Cases",
+                         line_color='blue'))
+
+italyGraph.add_trace(go.Scatter(x=italy.Date, y=italy['Deaths'], name="Deaths Cases",
+                         line_color='red'))
+
+italyGraph.add_trace(go.Scatter(x=italy.Date, y=italy['Recovered'], name="Recovered Cases",
+                         line_color='green'))
+
+italyGraph.update_layout(
+    title={
+        'text': "Time Series for Italy",
+        'y':0.9,
+        'x':0.45,
+        'xanchor': 'center',
+        'yanchor': 'top'})
+
+italyGraph.show()
